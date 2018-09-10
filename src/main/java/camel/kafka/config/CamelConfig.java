b1af2f24e940e55f1dbfa010c282d6367a2feb0b
mbarlot <mike.barlotta@walmart.com>
2018-09-10 18:39:05 -0400

    remove unneeded config

--- src/main/java/camel/kafka/config/CamelConfig.java
+++ src/main/java/camel/kafka/config/CamelConfig.java
@@ -1,7 +1,6 @@
 package camel.kafka.config;
 
 import camel.kafka.camel.KafkaConsumerRouteBuilder;
-import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.spring.javaconfig.CamelConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -14,11 +13,6 @@ import java.util.List;
 @Configuration
 @PropertySource(value = "classpath:application.properties")
 public class CamelConfig extends CamelConfiguration {
-
-    @Override
-    protected void setupCamelContext(CamelContext camelContext) throws Exception {
-        super.setupCamelContext(camelContext);
-    }
 
     /**
      * Camel routes
